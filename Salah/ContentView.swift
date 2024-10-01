abd55ca27c55bdf126bedcbd737d1265180a5dab
Monaalruthaya <monaalruthaya@outlook.com>
2024-10-01 21:23:45 +0300

    edit

--- Salah/ContentView.swift
+++ Salah/ContentView.swift
@@ -4,11 +4,12 @@
 //
 //  Created by Reem Alotaibi on 26/03/1446 AH.
 //
+/*
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
+       ZStack {
             // الصورة الأولى
             addImageView(imageName: "IshaButton", width: 100, height: 100)
                 .offset(x: 0, y: 0) // الموقع الأصلي
@@ -22,6 +23,7 @@ struct ContentView: View {
                 .offset(x: 0, y: 50) // تحريك الصورة لأسفل بمقدار 50 نقطة
         }
     }
+    
     //كود عشان يكون حكم الاستس مضبوط
     func addImageView(imageName: String, width: CGFloat, height: CGFloat) -> some View {
         Image(imageName)
@@ -29,10 +31,24 @@ struct ContentView: View {
             .scaledToFit()
             .frame(width: width, height: height)
     }
+    
 }
+ */
+import SwiftUI
 
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            // Add the background image
+            Image("background")
+                .resizable() // Make the image resizable
+                .aspectRatio(contentMode: .fill) // Fill the screen
+                .edgesIgnoringSafeArea(.all) // Ignore safe areas to cover the whole screen
 
-
+            
+        }
+    }
+}
 
 //كود اظهار السيميليتور الجانبي
 struct ContentView_Previews: PreviewProvider {
